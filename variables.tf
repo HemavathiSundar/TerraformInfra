13c48b266adbb5a1f01b29fdac46c3b789177ea0
HemavathiSundar <148438501+HemavathiSundar@users.noreply.github.com>
2024-03-22 11:30:39 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
+variable "infra_env" {
+     description = "infrastructure environment"
+}
+
+variable "subnet_id_value" {
+    description = "value for the subnet_id"
+}
