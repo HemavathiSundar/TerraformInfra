7425c010010da048cd5701e2bebc02e7ef6febc5
HemavathiSundar <148438501+HemavathiSundar@users.noreply.github.com>
2024-03-22 11:58:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "ami_value" {
-    description = "value for the ami"
-}
-
-variable "instance_type_value" {
-    description = "value for instance_type"
-}
-
-variable "infra_env" {
-     description = "infrastructure environment"
-}
-
-variable "subnet_id_value" {
-    description = "value for the subnet_id"
+variable "ec2_name" {
+  type = string
 }
